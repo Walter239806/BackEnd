d19c0e5471d2b9beaa21eb6f58084d66f7004981
Ubuntu <walter@Dev2.xsmgcfluo3ke5cbadtolfbtzzc.bx.internal.cloudapp.net>
2022-12-05 00:49:02 +0000

    Primer proyecto

---
+++ src/controllers/users.js
@@ -0,0 +1,22 @@
+export const TEST= (req, res, next) => {
+    try {
+        //a=null 
+        //throw new Error("error de entrada");
+        const input = req.body;
+        console.log('input=', input);
+
+        res.send(`Nombre ingresado: ${input.name}`);
+    } catch(error){
+
+        return next({
+            Code:501,
+            message:{
+                text : "Error 501",
+                params:{
+                    value:"01",
+                        }
+                    }
+            })
+        
+            }
+        }
---
+++ src/middlewares/basicErrorHandlers.js
@@ -0,0 +1,18 @@
+import {logger} from "../tools/basiclogs.js";
+
+
+export const errorHandler = (err,req, res, next)=> {
+    
+    logger.error("error message= ", err.message);
+    logger.error("error code= ", err.code);
+
+
+    //console.log("err.message = ", err.message);
+    //console.log("err.code = ", err.code);
+
+    return res.status(505).send({
+        errorCode: err.code, 
+        errorMessage: err.message, 
+    });
+
+}
