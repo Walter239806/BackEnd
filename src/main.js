bdeaccdb0c5de13ec604cae3bc6e601bb1b002b1
Ubuntu <walter@Dev2.xsmgcfluo3ke5cbadtolfbtzzc.bx.internal.cloudapp.net>
2022-12-05 00:46:34 +0000

    Primer proyecto

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
+import routes from "./routes/index.js";
+import {errorHandler} from "./middlewares/basicErrorHandlers.js"
+import { logger } from "./tools/basiclogs.js";
+
+//middleware
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(compression());
+app.use(helmet());
+app.use(routes);
+app.use(errorHandler);
+app.use (logger);
+
+process.on('uncaughtException', (error)=>{
+    logger.error('uncaughtException = ', error);
+    process.exit(1);
+  
+  })
+  
+  process.on('unhandledRejection', (error) => {
+    logger.error('unhandledRejection! :', error);
+    process.exit(1);
+  
+  })
+
+//Listener WebServer Express
+const NODE_PORT = 3001;
+app.listen(NODE_PORT, ()=>{
+    console.log(`Escuchando puerto ${NODE_PORT}`);
+
+});
+
+app.listen(NODE_PORT, () => {
+    console.log("port listening " + NODE_PORT);
+  
+    logger.info(`Listening to port ${NODE_PORT}`);
+ });
