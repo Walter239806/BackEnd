6160df8de30063f99238a606d1733535aa8c95e6
Walter <walter@Dev2.xsmgcfluo3ke5cbadtolfbtzzc.bx.internal.cloudapp.net>
2022-12-13 01:50:29 +0000

    test

--- src/main.js
+++ src/main.js
@@ -1,50 +1,43 @@
-import express from 'express';
-import helmet from 'helmet';
-import cors from 'cors';
-import compression from 'compression';
-import routes from "./routes/index.js";
-import {errorHandler} from "./middlewares/basicErrorHandlers.js";
-import { logger } from "./tools/basiclogs.js";
-import config from "./config/index.js";
-import database from "./database/index.js";
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import compression from 'compression'
+import routes from './routes/index.js'
+import { errorHandler } from './middlewares/basicErrorHandlers.js'
+import { logger } from './tools/basiclogs.js'
+import config from './config/index.js'
+import database from './database/index.js'
 
 //middleware
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
-app.use(compression());
-app.use(helmet());
-app.use(routes);
-app.use(errorHandler);
+app.use(express.json())
+app.use(cors())
+app.use(compression())
+app.use(helmet())
+app.use(routes)
+app.use(errorHandler)
 
+process.on('uncaughtException', error => {
+  logger.error('uncaughtException = ', error)
+  process.exit(1)
+})
 
-process.on('uncaughtException', (error)=>{
-    logger.error('uncaughtException = ', error);
-    process.exit(1);
-  
-  })
-  
-  process.on('unhandledRejection', (error) => {
-    logger.error('unhandledRejection! :', error);
-    process.exit(1);
-  
-  })
+process.on('unhandledRejection', error => {
+  logger.error('unhandledRejection! :', error)
+  process.exit(1)
+})
 
 //Listener WebServer Express
 //const NODE_PORT = 3001;
 //app.listen(NODE_PORT, ()=>{
-  //  console.log(`Escuchando puerto ${NODE_PORT}`);
+//  console.log(`Escuchando puerto ${NODE_PORT}`);
 
 //});
 
-database.setConnection().then(()=>{
-
+database.setConnection().then(() => {
   app.listen(config.NODE_PORT, () => {
     //console.log("port listening " + config.NODE_PORT);
-  logger.info(`Listening to port ${config.NODE_PORT}`);
- 
-
-});
-
+    logger.info(`Listening to port ${config.NODE_PORT}`)
+  })
 })
--- src/model/user.js
+++ src/model/user.js
@@ -1,30 +1,30 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const { Schema }= mongoose
-const userSchema = new Schema({
-
+const { Schema } = mongoose
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: [true, 'Please type a username'],
-        unique: true,
-    }, 
+      type: String,
+      required: [true, 'Please type a username'],
+      unique: true
+    },
 
     fullname: {
-        type: String,
-        required: [true, 'Please enter your name'],
-        unique: false,
+      type: String,
+      required: [true, 'Please enter your name'],
+      unique: false
     },
-    
+
     password: {
-        type: String, 
-        required: [true, 'Plese type a password'],
-        unique: false, 
-    },
-},
-    {
-        collection:"Users",
-        timestamps: true
+      type: String,
+      required: [true, 'Plese type a password'],
+      unique: false
+    }
+  },
+  {
+    collection: 'Users',
+    timestamps: true
+  }
+)
 
-    });
-
-    export default mongoose.model('Users', userSchema);
+export default mongoose.model('Users', userSchema)
