65e0aee5ee4040fca94056cd0ae425fc9918a6fc
Walter <walter@Dev2.xsmgcfluo3ke5cbadtolfbtzzc.bx.internal.cloudapp.net>
2023-01-09 23:46:56 +0000

    hola

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -6,12 +6,21 @@ import { MetaData } from './metadata.js'
 export const CREATE = async (req, res, next) => {
   try {
     const input = req.body
+    input.bitacora = {
+      user: input.author,
+      fecha: new Date()
+    }
+    input.metadata = {
+      hash: (Math.random() + 1).toString(36).substring(7),
+      num: Math.random()
+    }
+
     const newPost = new Model(input)
     const result = await newPost.save()
-    const { _id } = result
-    const username = input.author
-    await MetaData()
-    await bitacora(_id, username)
+    // const { _id } = result
+    // const username = input.author
+    // await MetaData()
+    // await bitacora(_id, username)
 
     res.send('ok')
   } catch (error) {
